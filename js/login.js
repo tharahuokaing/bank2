8b036a72f7a6fd6939d94be855f66c7ea46890f3
THARA HUOKAING <tharahuokaing@gmail.com>
2026-07-18 10:42:21 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -337,8 +337,8 @@ function initializeDashboardData() {
     
     // Assuming these elements exist in your updated dashboard
     if(document.getElementById("accountCount")) {
-        document.getElementById("accountCount").textContent = "10,000";
-        document.getElementById("depositTotal").textContent = "$152,910,000";
+        document.getElementById("accountCount").textContent = "15,000";
+        document.getElementById("depositTotal").textContent = "$252,910,000";
         document.getElementById("txCount").textContent = "40,000";
     }
 }
