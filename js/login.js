e32d72ba13b5be76d82b267806124a48a23702d8
THARA HUOKAING <tharahuokaing@gmail.com>
2026-07-19 15:40:56 +0700

    Fix formatting issue in login.js user data

--- js/login.js
+++ js/login.js
@@ -25,7 +25,7 @@
         { username: "mek", password: "mek", role: "Customer", requires2FA: false },
         { username: "test2", password: "test2", role: "Customer", requires2FA: false },
         { username: "test3", password: "test3", role: "Test Customer", requires2FA: false },
-        { username: "test4", password: "test4", role: "Test Customer", requires2FA: false }  
+        { username: "test4", password: "test4", role: "Test Customer", requires2FA: false },
         { username: "thorn", password: "thorn", role: "Customer", requires2FA: false },
         { username: "sansopheata", password: "sansopheata", role: "Chief Executive Officer", requires2FA: false },
         { username: "chansamnang", password: "chansamnang", role: "Customer", requires2FA: false },
