bc914842a36ede80a12408f10d307868bb109c5e
THARA HUOKAING <tharahuokaing@gmail.com>
2026-08-12 21:12:27 +0700

    Update total deposits amount in login.js

--- js/login.js
+++ js/login.js
@@ -200,7 +200,7 @@
                 <div class="dashboard-card">
                     <a href="https://tharahuokaing.github.io/deposit/" style="text-decoration: none; color: inherit; display: block; height: 100%; width: 100%;">
                         <img src="total_deposit.jpg" alt="Deposits Icon" class="card-icon" style="width:150px; height:150px;">
-                        <h3 id="depositTotal">$455,000,000</h3>
+                        <h3 id="depositTotal">$555,000,000</h3>
                         <p>Total Deposits</p>
                         <span class="card-link">View Details</span>
                     </a>
