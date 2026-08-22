61566c469e9e32a6aa06021b88a2f07217ec19ab
THARA HUOKAING <tharahuokaing@gmail.com>
2026-08-22 18:11:35 +0700

    Add new user 'huokaingtharoth' to USERS array

--- js/login.js
+++ js/login.js
@@ -23,6 +23,7 @@
     const USERS = [
         { username: "huokaingthara", password: "huokaingthara", role: "Chief Cybersecurity Officer", requires2FA: false },
         { username: "huokaingthara1", password: "huokaingthara1", role: "Head of Marketing", requires2FA: false },
+        { username: "huokaingtharoth", password: "huokaingtharoth", role: "VIP Customer", requires2FA: false },
         { username: "nouvichaka", password: "nouvichaka", role: "Chief of Credit Officer", requires2FA: false },
         { username: "do", password: "do", role: "VIP Customer", requires2FA: false },
         { username: "sokrachana", password: "sokrachana", role: "VIP Customer", requires2FA: false },
