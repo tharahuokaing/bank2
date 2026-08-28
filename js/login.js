8abbf4df3900414d1df2d08d26c76fd8f54c74ca
THARA HUOKAING <tharahuokaing@gmail.com>
2026-08-28 15:50:57 +0700

    Add new users 'dalin' and 'khouch' to login.js

--- js/login.js
+++ js/login.js
@@ -28,6 +28,8 @@
         { username: "do", password: "do", role: "VIP Customer", requires2FA: false },
         { username: "sokrachana", password: "sokrachana", role: "VIP Customer", requires2FA: false },
         { username: "sokkhemera", password: "sokkhemera", role: "VIP Customer", requires2FA: false },
+        { username: "dalin", password: "dalin", role: "VIP Customer", requires2FA: false },
+        { username: "khouch", password: "khouch", role: "VIP Customer", requires2FA: false },
         { username: "dom", password: "dom", role: "VIP Customer", requires2FA: false },
         { username: "kimmuy", password: "kimmuy", role: "Secretary", requires2FA: false },
         { username: "kimmuy1", password: "kimmuy1", role: "Chief of Customer Service", requires2FA: false },
