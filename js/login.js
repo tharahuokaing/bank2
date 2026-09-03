7e5caecb9c51cbdec69e22a1e09ee9e9f7fc0fc7
THARA HUOKAING <tharahuokaing@gmail.com>
2026-09-03 16:02:51 +0700

    Change role of user 'dom' to CEO

--- js/login.js
+++ js/login.js
@@ -31,7 +31,7 @@
         { username: "sokkhemera", password: "sokkhemera", role: "VIP Customer", requires2FA: false },
         { username: "dalin", password: "dalin", role: "VIP Customer", requires2FA: false },
         { username: "khouch", password: "khouch", role: "VIP Customer", requires2FA: false },
-        { username: "dom", password: "dom", role: "VIP Customer", requires2FA: false },
+        { username: "dom", password: "dom", role: "Chief Executive Officer", requires2FA: false },
         { username: "kimmuy", password: "kimmuy", role: "Secretary", requires2FA: false },
         { username: "kimmuy1", password: "kimmuy1", role: "Chief of Customer Service", requires2FA: false },
         { username: "jav", password: "jav", role: "Chief of Security", requires2FA: false },
